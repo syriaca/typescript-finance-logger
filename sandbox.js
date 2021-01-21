1165d2deba7a27a4944222c58f7eda48025e5c45
jean-yves le cabellec <jean-yves.lecabellec@leetchi.com>
2021-01-21 12:27:01 +0100

    Part#6 any and explicit types

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,22 @@
-var character = 'luigi';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+// arrays
+var ladies = [];
+ladies.push("hitomi tanaka");
+// union types
+var mixed = [];
+mixed.push("test", 50, true, {});
+console.log(mixed);
+// objects
+var object;
+object = {};
+console.log(object);
+// Any types
+var test = 25;
+test = [];
+console.log(test);
+// Any on object
+var ninja;
+ninja = { name: "yoshi", age: true };
--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,30 @@
-const character = 'luigi';
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
 
-console.log(character);
+// arrays
+let ladies: string[] = [];
+ladies.push("hitomi tanaka");
 
-const inputs = document.querySelectorAll('input');
+// union types
+let mixed: (string | number | boolean | object)[] = [];
+mixed.push("test", 50, true, {})
 
-console.log(inputs);
+console.log(mixed);
 
-inputs.forEach(input => {
-    console.log(input);
-})
+// objects
+let object: object;
+object = {};
+
+console.log(object);
+
+// Any types
+let test: any = 25;
+test = [];
+console.log(test);
+
+// Any on object
+let ninja: { name: any, age: (string | boolean) };
+
+ninja = {name: "yoshi", age: true}
